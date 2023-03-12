166bca35fcf98688eaff2983878b664d543f08b5
Erman01 <ekemanci@gmail.com>
2023-03-12 17:17:13 +0000

    Signin form validation completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,14 +9,33 @@ import { AuthenticationService } from '../service/authentication/authentication.
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  isFormInvalid = false;
+  areCredentialsInvalid = false;
+
   constructor(private authenticationService: AuthenticationService) {}
 
   onSubmit(signInForm: NgForm) {
+    if (!signInForm.valid) {
+      this.isFormInvalid = true;
+      this.areCredentialsInvalid = false;
+      return;
+    }
+    this.checkCredentials(signInForm);
     console.log(signInForm.value);
     const signInData = new SignInData(
       signInForm.value.username,
       signInForm.value.password
     );
     this.authenticationService.authenticate(signInData);
+  }
+  private checkCredentials(signInForm: NgForm) {
+    const signInData = new SignInData(
+      signInForm.value.username,
+      signInForm.value.password
+    );
+    if (!this.authenticationService.authenticate(signInData)) {
+      this.isFormInvalid = false;
+      this.areCredentialsInvalid = true;
+    }
   }
 }
