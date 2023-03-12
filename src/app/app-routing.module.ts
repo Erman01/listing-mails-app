5d4a3658e90556ee7b91a982548cbd6a14c45e12
Erman01 <ekemanci@gmail.com>
2023-03-12 16:53:49 +0000

    Auth guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { MailsComponent } from './mails/mails.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'mails', component: MailsComponent },
+  { path: 'mails', component: MailsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
